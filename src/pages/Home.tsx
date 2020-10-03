4a324f35c2be9b40526e09d6c0803a1bbd9452f8
Evan Pierce <mrthinger@gmail.com>
2020-10-02 21:13:46 -0400

    add base google login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { ItemComponenent } from "../components/ItemComponent";
 import { Item } from "../models/Item";
-import firebase from "../services/Firebase";
+import {Firebase} from "../services/Firebase";
 
 export const Home = () => {
   const [items, loading, error] = useCollectionData<Item>(
-    firebase.firestore().collection("items")
+    Firebase.firestore().collection("items")
   );
 
   return (
