0065657b7662a7a8d6dac95bb6c9b32fd3083031
Evan Pierce <mrthinger@gmail.com>
2020-10-02 20:11:22 -0400

    added firebase hooks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,18 @@
 import React from "react";
-import { useState } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 import { ItemComponenent } from "../components/ItemComponent";
 import { Item } from "../models/Item";
 import firebase from "../services/Firebase";
 
 export const Home = () => {
-  const [items, setItems] = useState<Item[]>([]);
-
-  firebase
-    .firestore()
-    .collection("items")
-    .onSnapshot((s) => {
-      const items = s.docs.map((e) => e.data() as Item);
-      setItems(items);
-    });
+  const [items, loading, error] = useCollectionData<Item>(
+    firebase.firestore().collection("items")
+  );
 
   return (
     <div>
       HomePage
-      {items.map((item) => (
-        <ItemComponenent item={item} />
-      ))}
+      {items && items.map((item) => <ItemComponenent item={item} />)}
     </div>
   );
 };
